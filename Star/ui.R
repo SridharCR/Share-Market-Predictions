fdfddb2d014175d2f33b82b8066a11147ff4e02c
SridharCR <crsridhar23@gmail.com>
2018-04-01 11:15:42 +0530

    second commit

--- Star/ui.R
+++ Star/ui.R
@@ -13,16 +13,20 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Stock prediction"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
+    
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      textInput("Stock", "Select any Stock", value="MSFT"),
+      selectInput('DayBack', "How much history you want to analyze?",
+                  choices=c("30 Days"="30", "120 Days"="120", "Maximum Number of Days Available"="Max"), selected="Max"),
+      sliderInput('DayForward',"How many days you want to predict:",min = 1,max = 210,step=1,sep="",value = 30),
+      checkboxGroupInput("Predictors", "Select the preferred ratio(Date is mandatory)", 
+                         choices=c("Date"="7", "Return On Assets (FP)"="12", "Return On Equity (FP)"="13", "Profit Margin (FP)"="14", 
+                                   "Current Ratio (FP)"="15", "Quick Ratio (FP)"="16", "Debt to Equity Ratio (FP)"="17", "Interest Coverage Ratio (FP)"="18", 
+                                   "Asset Turnover Ratio (FP)"="19", "Inventory Turnover Ratio (FP)"="20"), selected="7"),
     ),
     
     # Show a plot of the generated distribution
